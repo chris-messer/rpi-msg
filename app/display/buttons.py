2ac3d1a8795199e5526f894a90ac6c7d2b2333e6
chmesser <chmesser@deloitte.com>
2024-07-26 10:41:55 -0500

    Converted time to be CT time

--- app/display/buttons.py
+++ app/display/buttons.py
@@ -3,7 +3,7 @@ from app.display.print_to_eink import clear_screen
 from app.messaging.messages import send_message
 from dotenv import load_dotenv
 import logging
-from time import gmtime, strftime
+from time import gmtime, strftime, mktime, localtime
 
 
 
@@ -17,9 +17,11 @@ btn4 = Button(19)
 
 
 def key1():
-    ts = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    send_message('18179956114',f'Kali saw your image at {ts}')
-    send_message('18175019724',f'Kali saw your image at {ts}')
+    utc_time = gmtime()
+    cst_time = localtime(mktime(utc_time) - 6 * 3600)
+    ts = strftime("%Y-%m-%d %H:%M:%S", cst_time)
+    send_message('18179956114',f'Kali saw your image at {ts} CT')
+    send_message('18175019724',f'Kali saw your image at {ts} CT')
 
 def key2():
     clear_screen()
